1928131b9ea441450b266abac2ba9456ac730750
TanmeenPatel <tanmeenpatel@gmail.com>
2021-08-23 17:23:59 +0400

    added 2 folders :)

---
+++ assets/js/_vti_cnf/main.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Jan 2021 13:58:23 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Tanmeen\\ Patel\\ -\\ Autobiography/likedoing.html Tanmeen\\ Patel\\ -\\ Autobiography/elements.html Tanmeen\\ Patel\\ -\\ Autobiography/index.html Tanmeen\\ Patel\\ -\\ Autobiography/familyfriends.html Tanmeen\\ Patel\\ -\\ Autobiography/qualities.html Tanmeen\\ Patel\\ -\\ Autobiography/myimage.html Tanmeen\\ Patel\\ -\\ Autobiography/achievements.html
